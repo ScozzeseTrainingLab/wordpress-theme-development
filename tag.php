d24e0c34bed38b8bc77f51d28e4f3e9862d51dd5
Michele Salvini <michele@scozzese.com>
2016-04-18 19:45:40 +0200

    category/tag page

---
+++ tag.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+  <div class="row">
+
+    <?php
+
+      wp_nav_menu(array(
+        'menu' => 'main',
+        'theme_location' => 'main',
+        'depth' => 3,
+        'container' => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id' => 'bs-example-navbar-collapse-1',
+        'menu_class' => 'nav navbar-nav',
+        'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+        'walker' => new wp_bootstrap_navwalker()
+      ));
+
+    ?>
+
+  </div>
+
+
+  <div class="jumbotron">
+    <h1><?php single_term_title(); ?></h1>
+  </div>
+
+  <div class="row">
+
+    <?php
+
+    while (have_posts()) {
+      the_post();
+      get_template_part('parts/post');
+    }
+
+    ?>
+
+  </div>
+
+  <div class="row">
+
+    <?php get_template_part('parts/navigation'); ?>
+
+  </div>
+
+
+</div>
+
+
+<?php get_footer(); ?>
